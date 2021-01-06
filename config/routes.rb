82d1e47ad522349a073fe944130771dab78f7170
adifsgaid <infosgaid@gmail.com>
2021-01-06 14:19:03 +0100

    Created movie controller + react components

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  get 'movie/index'
   devise_for :users
-  get 'hello_world', to: 'hello_world#index'
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
