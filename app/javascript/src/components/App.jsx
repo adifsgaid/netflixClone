4cd1296a387a81161c403e23cc408f7cf6d85681
adifsgaid <infosgaid@gmail.com>
2021-01-08 15:27:56 +0100

    added Css and Axios to fetch movie from API

--- app/javascript/src/components/App.jsx
+++ app/javascript/src/components/App.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import '../../../assets/stylesheets/App.css'
+import Row from './Row'
 
 class App extends React.Component {
       render() {
         return (
+          <div className='App'> 
           <h1>Hello adif</h1>
+          <Row title='NETFLIX ORIGINAL'/>
+          <Row title='Trending Now '/>
+          </div>
         )
       }
     }
