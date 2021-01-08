08b24be090b32b45594e869d02f723ae80417af8
adifsgaid <infosgaid@gmail.com>
2021-01-08 15:58:45 +0100

    create Row an useEffect for loading

--- app/javascript/src/components/App.jsx
+++ app/javascript/src/components/App.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import '../../../assets/stylesheets/App.css'
 import Row from './Row'
+import requests from '../../services/requests'
 
 class App extends React.Component {
       render() {
         return (
           <div className='App'> 
           <h1>Hello adif</h1>
-          <Row title='NETFLIX ORIGINAL'/>
-          <Row title='Trending Now '/>
+          <Row title='NETFLIX ORIGINAL' fetchUrl={requests.fetchNetflixOriginals}/>
+          <Row title='Trending Now ' fetchUrl={requests.fetchTrending}/>
           </div>
         )
       }
--- app/javascript/src/components/Row.jsx
+++ app/javascript/src/components/Row.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from '../../services/axios'
 
-function Row({title}) {
+function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([])
-    
+    useEffect(() => {
+        async function fetchData() {
+            const request = axios.get(fetchUrl)
+            setMovies(request.data.results)
+            return request
+        }
+        fetchData()
+    }, [fetchUrl]);
+     
     return (
-        <div>
+        <div> 
             <h2>{title}</h2>
         </div>
     )
-}
+} 
 
 export default Row
