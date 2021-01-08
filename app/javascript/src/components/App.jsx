e837f4c81baa2838975baab69556468d6f6a13e1
adifsgaid <infosgaid@gmail.com>
2021-01-08 16:49:55 +0100

    cleaned the orientation and modif to css

--- app/javascript/src/components/App.jsx
+++ app/javascript/src/components/App.jsx
@@ -1,17 +1,20 @@
-import React, { Component } from 'react'
-import '../../../assets/stylesheets/App.css'
-import Row from './Row'
-import requests from '../../services/requests'
+import React, { Component } from "react";
+import "../../../assets/stylesheets/App.css";
+import Row from "./Row";
+import requests from "../../services/requests";
 
 class App extends React.Component {
-      render() {
-        return (
-          <div className='App'> 
-          <h1>Hello adif</h1>
-          <Row title='NETFLIX ORIGINAL' fetchUrl={requests.fetchNetflixOriginals}/>
-          <Row title='Trending Now ' fetchUrl={requests.fetchTrending}/>
-          </div>
-        )
-      }
-    }
-export default App
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello adif</h1>
+        <Row
+          title="NETFLIX ORIGINAL"
+          fetchUrl={requests.fetchNetflixOriginals}
+        />
+        <Row title="Trending Now " fetchUrl={requests.fetchTrending} />
+      </div>
+    );
+  }
+}
+export default App;
--- app/javascript/src/components/Row.jsx
+++ app/javascript/src/components/Row.jsx
@@ -1,33 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import axios from '../../services/axios'
+import React, { useState, useEffect } from "react";
+import axios from "../../services/axios";
+import "../../../assets/stylesheets/Row.css";
 
-const baseUrl = 'https://image.tmdb.org/t/p/original/';
+const baseUrl = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
-    const [movies, setMovies] = useState([])
-    useEffect(() => {
-        async function fetchData() {
-            const request = await axios.get(fetchUrl)
-            setMovies(request.data.results)
-            return request
-        }
-        fetchData()
-    }, [fetchUrl]);
-     
-    console.log(movies)
-    
-    return (
-        <div className='row'> 
-            <h2>{title}</h2>
-            
-            <div className='row__movies'>
-                {movies.map(movie =>(
-                    <img src={`${baseUrl}${movie.poster_path}`} alt={movie.name}/>
-                ))}
-            </div>
-        </div>
-    )
-} 
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-export default Row 
- 
+  console.log(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${baseUrl}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
