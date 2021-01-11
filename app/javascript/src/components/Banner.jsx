cec6d666dac26fa8ca159a6cc01c12caa4c145fc
adifsgaid <infosgaid@gmail.com>
2021-01-11 13:52:38 +0100

    added pop ups yt trailer

--- app/javascript/src/components/Banner.jsx
+++ app/javascript/src/components/Banner.jsx
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 import "../../../assets/stylesheets/Banner.css";
 import axios from "../../services/axios";
 import requests from "../../services/requests";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -22,7 +25,26 @@ function Banner() {
   function shadowDescription(str, el) {
     return str?.length > el ? str.substr(0, el - 1) + "..." : str;
   }
+  const opts = {
+    height: "500",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
+  const handleCLick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.original_name || movie?.original_title || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
   return (
     <header
       className="banner"
@@ -39,14 +61,18 @@ function Banner() {
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
         <div className="banner__buttons">
-          <button className="banner__button">Play</button>
+          <button className="banner__button" onClick={() => handleCLick(movie)}>
+            Play
+          </button>
           <button className="banner__button">List</button>
         </div>
         <h1 className="banner__description">
+          {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
           {movie?.overview}
           {shadowDescription(movie?.overview, 350)}
         </h1>
       </div>
+
       <div className="banner__shadow" />
     </header>
   );
--- app/javascript/src/components/Row.jsx
+++ app/javascript/src/components/Row.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "../../services/axios";
 import "../../../assets/stylesheets/Row.css";
-import Banner from "./Banner";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -16,13 +19,35 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "500",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleCLick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.original_name || movie?.original_title || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
         {movies.map((movie) => (
           <img
-            key={movie.id}
+            onClick={() => handleCLick(movie)}
+            key={movie?.id}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${baseUrl}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -31,6 +56,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
