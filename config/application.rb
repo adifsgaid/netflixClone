3325cb78d1d002dc5b21b291004f80fe411badca
adifsgaid <infosgaid@gmail.com>
2021-01-12 13:28:25 +0100

    changes to heroku

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require 'rails/test_unit/railtie'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.assets.initialize_on_precompile = false
 module Netflixclone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
